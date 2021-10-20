6aa9677ff28d14f4befb35b53126a8ae6d45f740
Alka107 <78424521+Alka107@users.noreply.github.com>
2021-10-20 13:35:27 +0530

    if else

--- if-else.py
+++ if-else.py
@@ -1,3 +1,4 @@
 if __name__ == '__main__':
-N = int(input())
-print(("Not weired " if N%2==0 and (N<=4 or N>20) else "") + "Weird")
+  
+  N = int(input())
+  print(("Not weired " if N%2==0 and (N<=4 or N>20) else "") + "Weird")
